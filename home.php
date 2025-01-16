4e57df09ff8ca6c7b984bb122f463d7b5f20098e
Manel Pando <mpandofernandez@gmail.com>
2025-01-16 15:54:40 +0100

    afegit fons imatge, començant css

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es" >
+    <head>
+    <meta charset="UTF-8">
+        <title>TecView</title>
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+        <link rel="stylesheet" href="./css/home.css">
+        <link rel="icon" href="./img/logo.ico">
+    </head>
+    <body>
+        <header>
+            <button>Log Out</button>
+        </header>
+        <main>
+            <h1>BIENVENIDO</h1>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
