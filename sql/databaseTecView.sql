938d0990ce7e9bcee4808b51167178f4fbb74ad4
Manel Pando <mpandofernandez@gmail.com>
2025-01-16 15:32:28 +0100

    fet en mysql

--- sql/databaseTecView.sql
+++ sql/databaseTecView.sql
@@ -2,14 +2,14 @@ CREATE DATABASE TecView;
 USE TecView;
 
 CREATE TABLE users (
-	iduser INT IDENTITY(1,1) PRIMARY KEY,
-	mail VARCHAR(40) UNIQUE,
-	username VARCHAR(16) UNIQUE,
-	passHash VARCHAR(60),
+	iduser INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	mail VARCHAR(40) NOT NULL UNIQUE,
+	username VARCHAR(16) NOT NULL UNIQUE,
+	passHash VARCHAR(60) NOT NULL,
 	userFirstName VARCHAR(60),
 	userLastName VARCHAR(120),
 	creationDate DATETIME,
 	removeDate DATETIME,
 	lastSignIn DATETIME,
-	active TINYINT
+	activat TINYINT(1)
 );
