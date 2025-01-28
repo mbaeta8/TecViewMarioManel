87bb3a4c5b1fc8d56e2e3d2911e37a13d7b664d4
Manel Pando <mpandofernandez@gmail.com>
2025-01-28 15:12:59 +0100

    LogoutArreglat

--- home.php
+++ home.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+use PDO;
+use PDOException;
+
+if (isset($_POST['logout'])) {
+    header('Location: index.php');
+    exit();
+}
+
+    function getDBConnection()
+    {
+        $connString = 'mysql:host=localhost;port=3335;dbname=TecView';
+        $user = 'root';
+        $pass = '';
+        $db = null;
+        try {
+            $db = new PDO($connString, $user, $pass, [PDO::ATTR_PERSISTENT => true]);
+        } catch (PDOException $e) {
+            echo "<p style=\"color:red;\">Error " . $e->getMessage() . "</p>";
+        } finally {
+            return $db;
+        }
+
+
+    }
+?>
 <!DOCTYPE html>
 <html lang="es" >
     <head>
@@ -6,18 +33,16 @@
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
         <link rel='stylesheet' href='css/home.css'>
         <link rel="icon" href="./img/logo.ico">
-        <script>
-            function logoutRedirect() {
-                window.location.href = "index.php";
-            }
-        </script>
     </head>
     <body>
         <header>
-            <button id="logout" onclick="logoutRedirect()">Log Out</button>
+            <form method="post" action="">
+                <button type="submit" name="logout" id="logout">Log Out</button>
+            </form>
         </header>
         <main>
-            <h1>BIENVENIDO</h1>
+
+            <h1>BIENVENIDO</h1><h1 <?php echo $_SESSION['username']?>></h1>
         </main>
         <footer>
 
