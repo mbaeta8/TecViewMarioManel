fdabf7d69a8d98d0576ace3b13516cabd00623b8
Manel Pando <mpandofernandez@gmail.com>
2025-02-25 15:09:38 +0100

    profile.php

---
+++ profile.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>TecView</title>
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="icon" href="./img/logo.ico">
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
